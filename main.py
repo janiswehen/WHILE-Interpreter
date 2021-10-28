9a3deadbb2a9ba73321a59a9cd8a3f14a57797da
Janis Wehen <janiswehen@gmail.com>
2021-10-28 18:59:28 +0200

    test

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from os import _exit
 from interpreter import Interpreter
 
-fileName = "MOD.WHILE"
+fileName = "x'.WHILE"
 fileObj = open(fileName, "r")
 prog = fileObj.read()
 fileObj.close()
-args = [11,2]
+args = [11,23]
 
 inter = Interpreter()
 
 if __name__ == "__main__":
     inter.setProg(prog)
-    test = True
-    fail1,fail2 = 0,0
-    for i in range(1,21):
-        for j in range(1,21):
-            inter.setInput([i,j])
-            res = inter.run()
-            print(str(i) + "%" + str(j) + "= " + str(i%j) + " | " + str(res))
-            if not i%j == res:
-                test = False
-                fail1,fail2 = i,j
-                break
+    inter.setInput([2])
+    res = inter.run()
+    print(res)
+    #test = True
+    #fail1,fail2 = 0,0
+    #for i in range(1,21):
+    #    for j in range(1,21):
+    #        inter.setInput([i,j])
+    #        res = inter.run()
+    #        print(str(i) + "%" + str(j) + "= " + str(i%j) + " | " + str(res))
+    #        if not i%j == res:
+    #            test = False
+    #            fail1,fail2 = i,j
+    #            break
 
-    if test:
-        print("Sucsess!")
-    else:
-        print("Failed at" + str(fail1) + ", " + str(fail2) + "!")
+    #if test:
+    #    print("Sucsess!")
+    #else:
+    #    print("Failed at" + str(fail1) + ", " + str(fail2) + "!")
